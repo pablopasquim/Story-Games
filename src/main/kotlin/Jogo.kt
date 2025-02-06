c093aaaff309b5c69fc68d62faa79521f7a2ef83
pablopasquim <pablo.pasquim7@gmail.com>
2025-02-06 10:29:10 -0300

    Feat: Create class "Meu jogo" and generate toString in class "Jogo"

--- src/main/kotlin/Jogo.kt
+++ src/main/kotlin/Jogo.kt
@@ -1,4 +1,17 @@
 package org.example
 
 class Jogo {
+
+    var titulo = ""
+    var capa = ""
+    var descricao = ""
+
+    override fun toString(): String {
+        return "Meu Jogo:'\n" +
+                "Titulo: $titulo \n" +
+                "Capa: $capa \n" +
+                "Descricao: $descricao \n"
+    }
+
+
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -14,5 +14,12 @@ fun main() {
         .send(request, HttpResponse.BodyHandlers.ofString())
     val json = response.body()
     println(json)
+
+    val meuJogo = Jogo()
+    meuJogo.titulo = "Counter-Strike 2"
+    meuJogo.capa = "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/730/ss_0f8cf82d019c614760fd20801f2bb4001da7ea77.1920x1080.jpg?t=1729703045"
+    meuJogo.descricao = "Ao longo de mais de duas décadas, o Counter‑Strike ofereceu uma experiência competitiva de elite, moldada por milhões de jogadores de todo o mundo. Agora, o próximo capítulo da história do CS está prestes a começar: Counter‑Strike 2."
+    println(meuJogo)
+
 }
 
